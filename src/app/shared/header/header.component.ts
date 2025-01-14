82d68b657b2698227ac98140f21390f1d9770482
David Kolosza <david.kolosza@gmx.de>
2025-01-14 19:43:45 +0100

    search 1st steps

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Temtem } from '../../models/temtem.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  data: Temtem[] = []; // Hier sind alle Temtem-Daten
+  searchResults: { name: string; type: string; location?: string; technique?: string; trait?: string }[] = [];
 
+  ngOnInit(): void {
+    // Fetched Data (Mock-Daten oder aus API laden)
+    this.data =
+      [
+        {
+          number: 1,
+          name: 'Mimit',
+          types: ['Digital'],
+          traits: ['Striking Transmog'],
+          locations: [{ location: 'Iwaba' }],
+          portraitWikiUrl: 'https://example.com/mimit.png', // Beispiel-URL
+          stats: [{ statName: 'HP', statValue: 50 }, { statName: 'Attack', statValue: 40 }] // Beispiel-Stats
+        },// Weitere Temtem-Daten
+      ];
+  }
+
+  onSearch(event: Event): void {
+    const input = event.target as HTMLInputElement; // Sicherstellen, dass es sich um ein HTMLInputElement handelt
+    const searchValue = input.value; // Zugriff auf den Wert
+    console.log('Suchbegriff:', searchValue);
+    // Hier kannst du die Filterlogik hinzufügen
+  }
 }
+
+
