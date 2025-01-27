ae3f9985e6ae1f67a0f2674812a5b36adfb2e6b9
David Kolosza <david.kolosza@gmx.de>
2025-01-27 20:08:06 +0100

    home top box

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h1>Welcome to TemDosia</h1>`,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'], // Wichtig: Pfad muss stimmen
   standalone: true,
 })
 export class HomeComponent {}
