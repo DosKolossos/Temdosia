4083a56d9e03708a0549372202d6582799a5d5ea
David Kolosza <david.kolosza@gmx.de>
2025-02-01 16:14:51 +0100

    next steps locations

--- src/app/locations-islands/locations-islands.component.ts
+++ src/app/locations-islands/locations-islands.component.ts
@@ -1,24 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute } from '@angular/router';
-
-// @Component({
-//   selector: 'app-locations-islands',
-//   standalone: true,
-//   templateUrl: './locations-islands.component.html',
-//   styleUrls: ['./locations-islands.component.scss']
-// })
-// export class LocationsIslandsComponent implements OnInit {
-//   islandName: string | null = null;
-
-//   constructor(private route: ActivatedRoute) {}
-
-//   ngOnInit(): void {
-//     this.route.paramMap.subscribe(params => {
-//       this.islandName = params.get('island');
-//       console.log('Aktuelle Insel:', this.islandName); // Debugging
-//     });
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FetchService } from '../fetch.service'; // Importiere den Service
